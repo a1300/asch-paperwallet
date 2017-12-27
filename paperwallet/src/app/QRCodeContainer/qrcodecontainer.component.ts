2820d2c173bb815a6092c667e5230f2d9e9d8ecc
a1300 <matthias-knopp@gmx.net>
2017-12-27 22:25:40 +0100

    Problems with loading qrcode package

--- paperwallet/src/app/QRCodeContainer/qrcodecontainer.component.ts
+++ paperwallet/src/app/QRCodeContainer/qrcodecontainer.component.ts
@@ -1,12 +1,35 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Input } from '@angular/core'
 import  { ngQrCode } from 'angular-qrcode';
+import { AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { qrcode } from 'qrcode';
 
 @Component({
     selector: 'qrcodecontainer',
     templateUrl: './qrcodecontainer.component.html',
     styleUrls: ['./qrcodecontainer.component.css']
 })
-export class QRCodeContainerComponent {
+export class QRCodeContainerComponent implements AfterViewInit  {
+    
+    privateKey: string = '';
 
-    constructor() {}
+    ngAfterViewInit(): void {
+        let canvas  = <HTMLCanvasElement>(document.getElementById('canvas'));
+        
+        if(canvas === undefined)
+            throw('canvas is undefined');
+
+        this.privateKey = 'Das ist ein Test';
+
+        qrcode.toCanvas(canvas, this.privateKey, function(error){
+            if(error)
+                console.error(error);
+            else
+                console.log('success');
+        });
+        
+    }
+    
+    @Input('myDir') id;
+    constructor() {
+    }
 }
