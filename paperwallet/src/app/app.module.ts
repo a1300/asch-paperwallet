cd37b22c3b9d8e2f72dc732f8cc683776440e93b
a1300 <matthias-knopp@gmx.net>
2017-12-31 04:32:55 +0100

    Added missing FormsModule

--- paperwallet/src/app/app.module.ts
+++ paperwallet/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 //qr component
@@ -22,7 +23,8 @@ import { PrivateKeyInputComponent } from './QRCodeContainer/PrivateKeyInput/priv
     PrivateKeyInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
